18b3e22d706aa19639363b45fb0dad8f822ae051
Kalium 19 <kaliumsoporte@gmail.com>
2026-02-12 02:43:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7837102107-41ca8",
+  "appId": "1:607157941153:web:4d083a830f7fbb252111e0",
+  "apiKey": "AIzaSyCK2eGzq7rdG1ki-G6Asz-TsTHWdkPe3Nc",
+  "authDomain": "studio-7837102107-41ca8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "607157941153"
+};
